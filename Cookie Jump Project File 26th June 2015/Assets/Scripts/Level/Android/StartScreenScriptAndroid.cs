39afe67b3863b5204454f7bfc390d031b38e9939
drapermovies <jepdraper@gmail.com>
2015-06-26 20:51:43 +0100

    Project Files - 26th June
    Updated to include the project files from June 26th, 2015.

---
+++ Cookie Jump Project File 26th June 2015/Assets/Scripts/Level/Android/StartScreenScriptAndroid.cs
@@ -0,0 +1,42 @@
+﻿//Made by Joel Draper for MansionGaming.
+using UnityEngine;
+using System.Collections;
+
+public class StartScreenScriptAndroid : MonoBehaviour {
+
+	static bool sawOnce = false;
+    static bool fellOnce = false;
+
+    public buttonsAndroid button;
+
+    public GameObject guiCanvas;
+
+	// Use this for initialization
+	void Start () {
+		if(!sawOnce && !fellOnce) {
+			GetComponent<SpriteRenderer>().enabled = true;
+			Time.timeScale = 0;
+            guiCanvas.SetActive(false);
+		}
+
+		sawOnce = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (button.paused == false) {
+            if (Input.touchCount > 0)
+            {
+                if (Time.timeScale == 0 && (Input.GetTouch(0).phase == TouchPhase.Began) && !fellOnce)
+                {
+                    Debug.Log("start screen fall");
+                    Time.timeScale = 1;
+                    GetComponent<SpriteRenderer>().enabled = false;
+                    guiCanvas.SetActive(true);
+                    fellOnce = true;
+                    Debug.Log("fell once");
+                }
+            }
+		}
+	}
+}
---
+++ Cookie Jump Project File 26th June 2015/Assets/Scripts/Level/PC/StartScreenScript.cs
@@ -0,0 +1,50 @@
+﻿//Made by Joel Draper for MansionGaming.
+using UnityEngine;
+using System.Collections;
+
+public class StartScreenScript : MonoBehaviour {
+
+	static bool sawOnce = false;
+    static bool fellOnce = false;
+
+    public buttons button;
+
+    public GameObject guiCanvas;
+
+	// Use this for initialization
+	void Start () {
+		if(!sawOnce && !fellOnce) {
+			GetComponent<SpriteRenderer>().enabled = true;
+			Time.timeScale = 0;
+            guiCanvas.SetActive(false);
+		}
+
+		sawOnce = true;
+
+        Debug.Log("start screen started");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (button.paused == false)
+        {
+            if (Input.GetMouseButtonDown(0) && !fellOnce)
+            {
+                Debug.Log("start screen fall");
+                Time.timeScale = 1;
+                GetComponent<SpriteRenderer>().enabled = false;
+                guiCanvas.SetActive(true);
+                fellOnce = true;
+                Debug.Log("fell once");
+            }
+            if (Input.GetMouseButtonDown(0) && fellOnce)
+            {
+                Debug.Log("player has already fallen");
+            }
+        }
+        if (button.paused == true)
+        {
+            Debug.Log("game is paused, -start screen");
+        }
+	}
+}
